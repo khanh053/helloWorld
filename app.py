5851bd61bad98d3f22fb4a0e5a6b86808ce48ca8
Khanh Nguyen <khanhnguyen@khanhs-macbook-air.local>
2026-02-18 10:40:58 -0500

    Created about.html page with personal details

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Khanh Nguyen!'
+    return 'Hello World from Khanh Nguyen! This is my first code change'
+@app.route('/hello')
+def hello_world1():  # put application's code here
+    return 'Hello World from Khanh Nguyen! This is my HTML'
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
